bc16daa0c4767d33502924a1ebc49569709a4233
TTG3333 <ngassamsimon@gmail.com>
2023-09-16 14:28:26 -0400

    Fixed gitignore catching happy files

--- data.py
+++ data.py
@@ -1,30 +1,47 @@
 import pandas as pd
+import os
 
 readingInterval = 2000 # each data point is a 2s recording
-filename = "data3"
-data = pd.read_csv(f"./{filename}.csv")
 
-data = data.apply(pd.to_numeric, errors='coerce')
-data["time"] = data["time"].astype("int64")
+def parseFile(filename):
+    if filename.endswith(".csv"):
+        filename = filename[:-4]
+    if not os.path.exists(f"./{filename}"):
+        os.mkdir(f"./{filename}")
+    data = pd.read_csv(f"./{filename}.csv")
 
-firstTime = data["time"].values[0]
+    data = data.apply(pd.to_numeric, errors='coerce')
+    data["time"] = data["time"].astype("int64")
 
-data["timeOffset"] = data["time"] - firstTime
+    firstTime = data["time"].values[0]
 
-dataPoints = []
+    data["timeOffset"] = data["time"] - firstTime
 
-n = 0
-while True:
-    dataPoint = data[n*readingInterval <= data["timeOffset"]]
-    dataPoint = dataPoint[dataPoint["timeOffset"] < (n+1)*readingInterval]
-    if len(dataPoint.index) <= 0: # stop when there are no more lines
-        break
-    dataPoints.append(dataPoint)
-    n += 1
+    dataPoints = []
 
-del dataPoints[-1]
+    n = 0
+    while True:
+        dataPoint = data[n*readingInterval <= data["timeOffset"]]
+        dataPoint = dataPoint[dataPoint["timeOffset"] < (n+1)*readingInterval]
+        if len(dataPoint.index) <= 0: # stop when there are no more lines
+            break
+        firstTimeDataPoint = dataPoint["time"].values[0]
+        dataPoint["timeOffset"] = dataPoint["time"] - firstTimeDataPoint
+        dataPoints.append(dataPoint)
+        n += 1
 
-for y, x in enumerate(dataPoints):
-    print(y)
-    print(x)
-    x.to_csv(f"./{filename}P{y}.csv", index=False)
+    del dataPoints[-1]
+
+    for y, x in enumerate(dataPoints):
+        print(y)
+        print(x)
+        x.to_csv(f"./{filename}/{filename}-Parsed{y}.csv", index=False)
+
+mode = input("file or folder? ")
+if mode == "file":
+    parseFile(input("Filename/path: "))
+elif mode  == "folder":
+    dirname = input("Folder name/path: ")
+    for filename in os.listdir(dirname):
+        fn = os.path.join(dirname, filename)
+        parseFile(fn)
