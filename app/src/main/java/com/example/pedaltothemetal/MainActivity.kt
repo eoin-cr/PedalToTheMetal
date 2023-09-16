62e1a0960a78cbeb15858e3039522dbe1d0c3865
TTG3333 <ngassamsimon@gmail.com>
2023-09-16 19:12:18 -0400

    Added rendering

--- app/src/main/java/com/example/pedaltothemetal/MainActivity.kt
+++ app/src/main/java/com/example/pedaltothemetal/MainActivity.kt
@@ -1,43 +1,85 @@
-//package com.example.accelerometerstorer
-//
-//import android.os.Bundle
-//import androidx.activity.ComponentActivity
-//import androidx.activity.compose.setContent
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.material3.MaterialTheme
-//import androidx.compose.material3.Surface
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.tooling.preview.Preview
-//import com.example.accelerometerstorer.ui.theme.AccelerometerStorerTheme
-//
-//class MainActivity : ComponentActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            AccelerometerStorerTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-//                    Greeting("Android")
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//            text = "Hello $name!",
-//            modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    AccelerometerStorerTheme {
-//        Greeting("Android")
-//    }
-//}
+package com.example.pedaltothemetal
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.accelerometerstorer.R
+import com.example.pedaltothemetal.ui.theme.AccelerometerStorerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AccelerometerStorerTheme {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(0.15f)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.logo_htn),
+                            contentDescription = "HTN logo"
+                        )
+                        Spacer(modifier = Modifier.width(50.dp))
+                        Image(
+                            painter = painterResource(id = R.drawable.logo_2),
+                            contentDescription = "Logo 2"
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Mood("Happy", Modifier.align(CenterHorizontally))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Mood(mood: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = "Your current mood is",
+            fontSize = 32.sp
+        )
+        Text(
+            text = "$mood",
+            fontSize = 48.sp,
+            modifier = Modifier.align(CenterHorizontally)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AccelerometerStorerTheme {
+        Column {
+            Mood("Happy")
+            Image(
+                painter = painterResource(R.drawable.logo_1),
+                contentDescription = "Logo 1"
+            )
+        }
+    }
+}
