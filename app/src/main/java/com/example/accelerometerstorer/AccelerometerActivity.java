5ae4b9aed8d24ecb68c6a32cefafe670f8e172ba
Eoin <79023828+eoin-cr@users.noreply.github.com>
2023-09-16 10:11:33 -0400

    Using linear accel now, and created a button that doesn't do anything

--- app/src/main/java/com/example/accelerometerstorer/AccelerometerActivity.java
+++ app/src/main/java/com/example/accelerometerstorer/AccelerometerActivity.java
@@ -1,12 +1,19 @@
 package com.example.accelerometerstorer;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccelerometerActivity extends Activity implements SensorEventListener {
     private SensorManager sensorManager;
@@ -14,6 +21,9 @@ public class AccelerometerActivity extends Activity implements SensorEventListen
     TextView xCoor; // declare X axis object
     TextView yCoor; // declare Y axis object
     TextView zCoor; // declare Z axis object
+    List<Float[]> previousAccelerations = new ArrayList<>();
+    String previousAccelerationString = "";
+    TextView previousAccelerationsTextView;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -28,7 +38,7 @@ public class AccelerometerActivity extends Activity implements SensorEventListen
         sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
         // add listener. The listener will be  (this) class
         sensorManager.registerListener(this,
-                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION),
                 SensorManager.SENSOR_DELAY_NORMAL);
 
 
@@ -41,16 +51,31 @@ public class AccelerometerActivity extends Activity implements SensorEventListen
     public void onSensorChanged(SensorEvent event){
 
         // check sensor type
-        if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+        if(event.sensor.getType()==Sensor.TYPE_LINEAR_ACCELERATION){
 
             // assign directions
             float x=event.values[0];
             float y=event.values[1];
             float z=event.values[2];
+//            previousAccelerations.add(new Float[]{x, y, z});
 
             xCoor.setText("X: "+x);
             yCoor.setText("Y: "+y);
             zCoor.setText("Z: "+z);
+//            previousAccelerationString += previousAccelerations.get(previousAccelerations.size()-1);
+//            previousAccelerationsTextView.setText(previousAccelerationString);
+
+        }
+    }
+
+    private void writeToFile(String data, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
         }
     }
 }
