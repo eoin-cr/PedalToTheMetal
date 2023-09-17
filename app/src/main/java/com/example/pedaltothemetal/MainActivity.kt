45abbf8917338dc4317f91e3fb7ecf01daea8008
TTG3333 <ngassamsimon@gmail.com>
2023-09-16 20:59:54 -0400

    Some UI improvements

--- app/src/main/java/com/example/pedaltothemetal/MainActivity.kt
+++ app/src/main/java/com/example/pedaltothemetal/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,19 +31,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AccelerometerStorerTheme {
+            AccelerometerStorerTheme() {
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     Row(
                         horizontalArrangement = Arrangement.Center,
                         modifier = Modifier
                             .fillMaxWidth()
                             .fillMaxHeight(0.15f)
+                            .padding(top = 10.dp)
                     ) {
                         Image(
                             painter = painterResource(id = R.drawable.logo_htn),
                             contentDescription = "HTN logo"
                         )
-                        Spacer(modifier = Modifier.width(50.dp))
+                        Spacer(modifier = Modifier.width(100.dp))
                         Image(
                             painter = painterResource(id = R.drawable.logo_2),
                             contentDescription = "Logo 2"
@@ -59,7 +62,7 @@ class MainActivity : ComponentActivity() {
 fun Mood(mood: String, modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         Text(
-            text = "Your current mood is",
+            text = stringResource(id = R.string.mood_is),
             fontSize = 32.sp
         )
         Text(
