a51ba21234415b7dfec17e5853468cdfb9d28479
Eoin <79023828+eoin-cr@users.noreply.github.com>
2023-09-16 20:00:31 -0400

    Removed some bugs, still lots of issues

--- model.py
+++ model.py
@@ -13,6 +13,7 @@ from torch import optim, nn, utils, Tensor
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 import lightning.pytorch as pl
+from sklearn import preprocessing
 
 # define any number of nn.Modules (or use your current ones)
 encoder = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
@@ -65,24 +66,38 @@ train_emotion = []
 # for directory, subdirectories, files in os.walk(path):
 for directory, subdirectories, files in os.walk(path):
     for file in files:
-        if "Parsed" in file:
+        if "Parsed" in file and directory != "Test":
             for emotion in emotions:
                 if emotion in file:
                     # csvMap[key].append(pd.read_csv)
                     # data_df = pd.read_csv(os.path.join(directory, file))
                     # data_tensor = torch.tensor(data_df.values, dtype=torch.float32)
                     data = numpy.genfromtxt(os.path.join(directory, file), dtype=float, delimiter=',', names=True)
-                    # np.pad(data, (45-len(data), [0,0,0,0,0]), 'constant')
+                    # print(data)
+                    target_array_shape = (45, 4)
+                    pad_x = (target_array_shape[0]-data.shape[0])
+                    # print(f'{pad_x}, {data.shape[0]}')
 
-                    print(len(data))
+
+                    # data = np.pad(data, pad_width=((45-len(data), 0), (0, 0)))
+                    data = [list(item) for item in data]
+                    if len(data[0]) == 5:
+                        print(data)
+                    data = np.pad(data, ((pad_x, 0), (0,0)), mode="constant")
+
+                    # print(len(data))
                     train_acceleration.append(data)
                     train_emotion.append(emotion)
                     # csvMap[emotion].append(data_tensor)
                     break
 
+train_acceleration = np.array(train_acceleration)
 # print(train_acceleration)
+print(f'{len(train_acceleration)}, {len(train_emotion)}')
 tensor_acceleration = torch.Tensor(train_acceleration)
-tensor_emotion = torch.Tensor(train_emotion)
+le = preprocessing.LabelEncoder()
+targets = le.fit_transform(emotions)
+tensor_emotion = torch.Tensor(targets)
 
 # for emotion in csvMap.keys():
 #     for i in range(len(csvMap[emotion])):
