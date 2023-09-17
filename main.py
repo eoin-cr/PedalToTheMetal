1cc1dd919170cd19620943acefb1369dc61ae662
TTG3333 <ngassamsimon@gmail.com>
2023-09-17 00:51:42 -0400

    improved main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import os
+import json
 
-def playlistGeneration(genres): # Ex.) genres = ["hip-hop", "rainy-day"]
-	client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv("CLIENT_ID"), client_secret=os.getenv("SECRET_KEY"))
-	sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-	
-	results = sp.recommendations(seed_genres=genres, limit=20)
-	
-	res = results['tracks']
-	
-	for item in res:
-		print(item['name'], "-", item['artists'][0]['name'])
+with open("./env.json", "tr", encoding="utf-8") as F:
+    env = json.load(F)
 
+def playlistGeneration(genres):  # Ex.) genres = ["hip-hop", "rainy-day"]
+    if len(genres) > 5:
+        genres = genres[:5]
+    client_credentials_manager = SpotifyClientCredentials(client_id=env["CLIENT_ID"], client_secret=env["SECRET_KEY"])
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    results = sp.recommendations(seed_genres=genres, limit=20)
+
+    songs = results['tracks']
+
+    for idx, album in enumerate(songs):
+        print(album['name'], "-", album['artists'][0]['name'])
