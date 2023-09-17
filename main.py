aafb51be1d32fc79d73db6c10a0bc0ffc789b342
Derrick Ratnaharan <derrickratnaharan@v1044-wn-rt-a-142-145.campus-dynamic.uwaterloo.ca>
2023-09-16 23:25:31 -0400

    playlist generation

---
+++ main.py
@@ -0,0 +1,14 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import os
+
+def playlistGeneration(genres): # Ex.) genres = ["hip-hop", "rainy-day"]
+	client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv("CLIENT_ID"), client_secret=os.getenv("SECRET_KEY"))
+	sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+	
+	results = sp.recommendations(seed_genres=genres, limit=20)
+	
+	res = results['tracks']
+	
+	for item in res:
+		print(item['name'], "-", item['artists'][0]['name'])
